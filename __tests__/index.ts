e487f46666a83115d3e373c401d74d6d4c0e7aec
yayoc <n.yayoshi+github@gmail.com>
2018-04-14 19:13:35 +0530

    Bug fix Support accessing to undefined property at top level

--- __tests__/index.ts
+++ __tests__/index.ts
@@ -2,7 +2,7 @@ import { optional } from "../src";
 
 describe("optional-chain", () => {
   describe("object", () => {
-    it("should return undefined when accessing undefined property", () => {
+    it("should return undefined when accessing to undefined property at inner most level", () => {
       const obj = {
         a: {
           b: {
@@ -13,14 +13,60 @@ describe("optional-chain", () => {
       const optionalObj = optional(obj);
       expect((optionalObj.a.b as any).d).toBeUndefined();
     });
+    it("should return undefined when accessing to undefined property at top level", () => {
+      const obj = {};
+      const optionalObj = optional(obj) as any;
+      expect(optionalObj.a).toBeUndefined();
+    });
+    it("should return undefined when accessing to undefined property at middle level", () => {
+      const obj = {
+        a: {
+          b: {
+            c: "bar"
+          }
+        }
+      };
+      const optionalObj = optional(obj) as any;
+      expect(optionalObj.a.z).toBeUndefined();
+    });
+    it("should return undefined when accessing to unknown index at middle level", () => {
+      const obj = { a: [] };
+      const optionalObj = optional(obj) as any;
+      expect(optionalObj.a[1]).toBeUndefined();
+    });
+
+    it("should return proper values", () => {
+      const obj = {
+        a: {
+          b: {
+            c: "foo",
+            d: "bar"
+          },
+          e: [
+            {
+              f: 100
+            }
+          ]
+        }
+      };
+      const optionalObj = optional(obj);
+      expect(optionalObj.a.b.c).toBe("foo");
+      expect(optionalObj.a.b.d).toBe("bar");
+      expect(optionalObj.a.e[0].f).toBe(100);
+    });
   });
   describe("array", () => {
-    it("should return undefined when accessing undefined property", () => {
+    it("should return undefined when accessing to unknown index", () => {
       const arr = {
         a: [{ b: "foo" }]
       };
       const optionalArr = optional(arr);
       expect(optionalArr[10].b).toBeUndefined();
+    });
+    it("should return undefined when accessing undefined property", () => {
+      const arr = [];
+      const optionalArr = optional(arr);
+      expect(optionalArr[1]).toBeUndefined();
     });
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,14 @@
 export const optional = <T extends object>(obj: T): T => {
+  const isEmpty = (target: T): boolean => {
+    if (Array.isArray(target) && target.length === 0) {
+      return true;
+    }
+    return Object.keys(target).length === 0;
+  };
   const handler = {
     get: (target: T, p: PropertyKey): any => {
       if (!(<any>target)[p]) {
+        if (isEmpty(target)) return undefined;
         return {};
       }
       return Reflect.get(target, p);
